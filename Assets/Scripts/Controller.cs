f4c6b9b30bba6479b4ae2cd8875dbf0ef15b044f
Stephen Karpinskyj <stephen.karpinskyj@gmail.com>
2016-08-02 21:12:28 +1200

    Tweaked feel + added basic targeting

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 using Random = UnityEngine.Random;
+using System.Collections;
 
 public class Controller : MonoBehaviour
 {
+    private enum Direction
+    {
+        CW = 0,
+        CCW,
+    }
+
     [Serializable]
     private struct ColourPair
     {
@@ -20,6 +27,18 @@ public class Controller : MonoBehaviour
     private float angularSpeed = 2f;
 
     [SerializeField]
+    private Direction initialOrbitDirection = Direction.CW;
+
+    [SerializeField]
+    private Transform core;
+
+    [SerializeField]
+    private Transform cwOrbit;
+
+    [SerializeField]
+    private Transform ccwOrbit;
+
+    [SerializeField]
     private ColourPair[] colours;
 
     [SerializeField]
@@ -31,38 +50,106 @@ public class Controller : MonoBehaviour
     [SerializeField]
     private TrailRenderer trailRenderer;
 
-    private bool isTurning;
+    [SerializeField]
+    private Transform point;
+
+    [SerializeField]
+    private Vector2 pointBounds;
+
+    [SerializeField]
+    private float toOrbitDeltaSpeed = 5f;
+
+    [SerializeField]
+    private float fromOrbitDeltaSpeed = -20f;
+
+    private bool isOrbiting;
+    private Direction orbitDirection;
+    private float percentOrbit;
 
     private void Awake()
     {
         this.InitFramerate();
+
+        this.orbitDirection = this.initialOrbitDirection;
     }
 
     private void Start()
     {
-        this.UpdateColours();
-    }
+        this.CheckInput();
 
+        this.GenerateColours();
+        this.GeneratePoint();
+
+        this.StartCoroutine(this.RandomisePointCoroutine());
+    }
+    
     private void Update()
     {
         this.CheckInput();
     }
-
+    
     private void FixedUpdate()
     {
         this.CheckInput();
 
-        if (this.isTurning)
+        this.UpdatePercentOrbit(this.isOrbiting);
+
+        this.UpdateOrbit(this.orbitDirection);
+        this.UpdateForward();
+    }
+    
+    private IEnumerator RandomisePointCoroutine()
+    {
+        while (true)
         {
-            this.transform.localRotation *= Quaternion.AngleAxis(this.angularSpeed * Time.fixedDeltaTime, Vector3.up);
+            yield return new WaitForSeconds(5f);
+            this.GeneratePoint();
+        }
+    }
+    
+    private void UpdatePercentOrbit(bool increase)
+    {
+        var mag = increase ? this.toOrbitDeltaSpeed : this.fromOrbitDeltaSpeed;
+        var delta = mag * Time.fixedDeltaTime;
+        this.percentOrbit = Mathf.Clamp01(this.percentOrbit + delta);
+    }
+
+    private void UpdateOrbit(Direction direction)
+    {
+        var orbitPoint = direction == Direction.CW ? this.cwOrbit.position : this.ccwOrbit.position;
+        var orbitAmount = this.angularSpeed * Time.fixedDeltaTime * this.percentOrbit;
+
+        if (direction == Direction.CCW)
+        {
+            orbitAmount *= -1;
         }
 
-        this.transform.localPosition += this.transform.forward * Time.fixedDeltaTime * this.speed;
+        this.core.RotateAround(orbitPoint, Vector3.forward, orbitAmount);
+    }
+
+    private void UpdateForward()
+    {
+        this.core.localPosition += this.core.forward * Time.fixedDeltaTime * this.speed * (1 - this.percentOrbit);
     }
 
     private void CheckInput()
     {
-        this.isTurning = Input.anyKey;
+        var wasOrbiting = this.isOrbiting;
+        this.isOrbiting = Input.anyKey;
+
+        if (this.isOrbiting && !wasOrbiting)
+        {
+            this.OnStartOrbit();
+        }
+    }
+
+    private void OnStartOrbit()
+    {
+        var right = this.core.right;
+        var to = this.point.localPosition - this.core.localPosition;
+        var dot = Vector3.Dot(right, to);
+
+        this.orbitDirection = dot > 0f ? Direction.CCW : Direction.CW;
     }
 
     private void InitFramerate()
@@ -80,7 +167,7 @@ public class Controller : MonoBehaviour
         Debug.Log("FPS set to " + newFps);
     }
 
-    private void UpdateColours()
+    private void GenerateColours()
     {
         var newColours = this.colours[Random.Range(0, this.colours.Length)];
 
@@ -88,6 +175,11 @@ public class Controller : MonoBehaviour
 
         this.trailRenderer.material.color = newColours.ForegroundColour;
         this.spriteRenderer.material.color = newColours.ForegroundColour;
+    }
+
+    private void GeneratePoint()
+    {
+        this.point.localPosition = new Vector2(Random.Range(-pointBounds.x, pointBounds.x), Random.Range(-pointBounds.y, pointBounds.y));
     }
 
     public void UGUI_OnResetButtonPress()
